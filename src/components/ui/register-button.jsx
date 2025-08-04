228055af04bc438a46103c86d93eb32dc21bd65c
Atul-ThakreLO <atulthakrelrat3094@gmail.com>
2025-08-04 10:43:50 +0530

    fix minor issues

--- src/components/ui/register-button.jsx
+++ src/components/ui/register-button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const RegisterButton = () => {
   return (
-    <button className="relative drop-shadow-[0_0_20px_#f72561] border-pink-100 border-2  bg-pink-800 hover:bg-pink-900 text-pink-200 font-squid cursor-pointer font-bold tracking-widest px-10 py-5 rounded-lg text-xl transition-colors duration-300 overflow-hidden w-72 h-16 group">
+    <div className="relative drop-shadow-[0_0_20px_#f72561] border-pink-100 border-2  bg-pink-800 hover:bg-pink-900 text-pink-200 font-squid cursor-pointer font-bold tracking-widest px-10 py-5 rounded-lg text-xl transition-colors duration-300 overflow-hidden w-72 h-16 group">
       <span className="absolute inset-0 flex items-center justify-center transition-transform duration-300 ease-in-out group-hover:-translate-x-full">
         Register Now
       </span>
@@ -17,7 +17,7 @@ const RegisterButton = () => {
           priority
         />
       </span>
-    </button>
+    </div>
   );
 };
 
