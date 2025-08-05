6bc8292b7fc500a59edd3b9f88909b4b75375588
Atul-ThakreLO <atulthakrelrat3094@gmail.com>
2025-08-05 11:07:15 +0530

    Revert "offline registration"
    This reverts commit 7635e834ec727ace9dccb4c302196aea0d47ea9d.

--- next.config.mjs
+++ next.config.mjs
@@ -19,23 +19,14 @@ const nextConfig = {
     // modern: true,
   },
   // next.config.js
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/smackathon', 
-  //       permanent: false, // `false` for temporary (302), `true` for permanent (308)
-  //     },
-  //   ];
-  // },
   async redirects() {
     return [
       {
-        source: '/((?!smackathon).*)', // Match all paths except /smackathon
-        destination: '/smackathon',
-        permanent: false,
+        source: '/',
+        destination: '/smackathon', 
+        permanent: false, // `false` for temporary (302), `true` for permanent (308)
       },
-    ]
+    ];
   },
 
 };
