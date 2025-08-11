d0ffdef0669d53e9dac2ea8aeaa68aed42d794bf
Yogesh Nagrare <yogeshnagrare2004@gmail.com>
2025-08-11 19:13:15 +0530

    navbar and sponsers update added

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,8 @@ import Footer from "@/components/Home/Footer/Footer.jsx";
 import Hero from "@/components/Home/Hero/hero";
 import { FooterContactDetailsCosmos } from "../../data";
 import Events from "@/components/Home/Events/events";
+import SponsorSlider from "@/components/Home/Sponsors/SponsorSlider";
+
 
 export default function Home() {
   return (
@@ -10,6 +12,7 @@ export default function Home() {
       <Hero />
       <div className="relative z-10">
         <Events />
+        <SponsorSlider/>
         <Footer footerDetails={FooterContactDetailsCosmos} />
       </div>
     </div>
