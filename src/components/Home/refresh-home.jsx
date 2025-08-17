7718b4130414de4476529edd038ed32cb8ff3946
Atul-ThakreLO <atulthakrelrat3094@gmail.com>
2025-08-17 17:28:18 +0530

    animation fix

--- src/components/Home/refresh-home.jsx
+++ src/components/Home/refresh-home.jsx
@@ -1,27 +1,13 @@
-"use client"
+"use client";
 import React, { useEffect } from "react";
 
 const RefreshHome = () => {
   useEffect(() => {
-    let hasBlurred = false;
-
-    const handleBlur = () => {
-      hasBlurred = true;
+    const handleReload = () => {
+      console.log("reload");
+      window.location.reload();
     };
-
-    const handleFocus = () => {
-      if (hasBlurred) {
-        window.location.reload();
-      }
-    };
-
-    window.addEventListener("blur", handleBlur);
-    window.addEventListener("focus", handleFocus);
-
-    return () => {
-      window.removeEventListener("blur", handleBlur);
-      window.removeEventListener("focus", handleFocus);
-    };
+    window.addEventListener('popstate', handleReload);
   }, []);
 
   return null;
