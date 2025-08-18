d8cd9ef1319a4ca9e6f4e9f03412481a05f0161f
Atul-ThakreLO <atulthakrelrat3094@gmail.com>
2025-08-19 01:17:37 +0530

    route change bug

--- src/components/Home/refresh-home.jsx
+++ src/components/Home/refresh-home.jsx
@@ -1,18 +1,7 @@
 "use client";
-import { usePathname } from "next/navigation";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
 const RefreshHome = () => {
-  const pathname = usePathname();
-  const prevPath = useRef("");
-
-  useEffect(() => {
-    if (prevPath.current && pathname === "/") {
-      window.location.reload();
-    }
-    prevPath.current = pathname;
-  }, [pathname]);
-
   useEffect(() => {
     const handleReload = () => {
       window.location.reload();
